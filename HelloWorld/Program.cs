be2b484b0dda379cdef099993d13ea71183b3917
Kushal Pathak <kushalpathak80@gmail.com>
2024-05-09 17:26:26 +0545

    added first console app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+ namespace Basic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            //if (name == NULL) name = "Guest";
+            Console.WriteLine("Hello " + name + " how are you?");
+        }
+    }
+}
